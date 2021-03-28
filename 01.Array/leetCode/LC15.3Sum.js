f9931bd6cb5c1cad9ea412d3176c773f0d534aa3
Steve Leng <steve.leng@gmail.com>
2021-03-28 11:26:44 +0800

    update test files

---
+++ 01.Array/leetCode/LC15.3Sum.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/3sum/
+// https://www.youtube.com/watch?v=2tbi1W7ce1c
+
+let solution = function (nums) {
+  let result;
+
+  return result;
+};
+
+/*
+  1. 題目提示:
+      let nums1 = [-1, 0, 1, 2, -1, -4];
+      let nums1Ans = [
+        [-1, 0, 1],
+        [-1, -1, 2]
+      ];
+
+  2. 思路:
+                a + b + c = 0
+      可轉為:       a + b = -c
+
+  3. 思考:
+      -1 + 0 = -(-1)  (存在)
+      -1 + 1 = -(0)   (不存在)
+      -1 + 2 = -(-1)  (存在)
+
+*/
---
+++ 01.Array/leetCode/__test__/LC70.3Sum.test.js
@@ -0,0 +1,5 @@
+const solution = require('../LC15.3Sum');
+
+test('測試 climbStairs', () => {
+  expect(climbStairs(1)).toEqual(1);
+});
